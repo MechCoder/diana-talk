5278d497a47f6d6cef9923950ee05d7bd2ba3930
MechCoder <mks542@nyu.edu>
2017-02-01 18:56:16 -0500

    Update

--- optimizer.py
+++ optimizer.py
@@ -19,12 +19,29 @@ optimizer = Optimizer(
     acq_func="LCB",
     random_state=0)
 
-# Tell some points to the optimizer.
+# Tell some points to the optimizer and ask for the next point.
 X = np.reshape(np.linspace(4, 6, 10), (-1, 1)).tolist()
 y = [black_box(xi) for xi in X]
 optimizer.tell(X, y)
 x_cand = optimizer.ask()
 y_cand = black_box(x_cand)
+plot = plot_space(X, y, optimizer.models[-1], x_cand)
+plot.show()
 
+# Tell and ask again.
+optimizer.tell(x_cand, y_cand)
+X = X + [x_cand]
+y = y + [y_cand]
+x_cand = optimizer.ask()
+y_cand = black_box(x_cand)
+plot = plot_space(X, y, optimizer.models[-1], x_cand)
+plot.show()
+
+# Tell and ask again.
+optimizer.tell(x_cand, y_cand)
+X = X + [x_cand]
+y = y + [y_cand]
+x_cand = optimizer.ask()
+y_cand = black_box(x_cand)
 plot = plot_space(X, y, optimizer.models[-1], x_cand)
 plot.show()
